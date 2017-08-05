86be0af3d4da0c57835921ba52dccb0ebc94d767
cfsanderson <calebsanderson@gmail.com>
2017-08-05 08:10:55 -0400

    Adds HTML-and-PHP directory and files

--- HTML-and-PHP/index.php
+++ HTML-and-PHP/index.php
@@ -1,18 +1,23 @@
+<?php
+date_default_timezone_set('America/New_York');
+$day = date('N');
+$name = 'Caleb Frost Sanderson';
+?>
 <!DOCTYPE html>
 <html>
   <head>
   	<meta charset=utf-8>
-  	<title>PHP Basics at Treehouse</title>
+  	<title>PHP Basics by <?php echo $name; ?></title>
   	<link href="css/style.css" rel="stylesheet" />
   </head>
-  
+
   <body>
     <div id="wrap">
         <section class="sidebar text-center">
           <div class="avatar">
             <img src="img/logo.png" alt="Alt For Image">
           </div>
-          <h1>Name</h1>
+          <h1><?php echo $name; ?></h1>
           <p>Contact:<br />
           <a href="mailto:">EMAIL</a></p>
           <hr />
@@ -24,16 +29,16 @@
         </section>
         <section class="main">
           <h1>My First PHP Page</h1>
-    
+
           <h2>Unit Conversion</h2>
-    
+
           <hr />
-    
+
           <h2>Daily Exercise</h2>
         </section>
     </div>
     <section class="footer text-center">
-      &copy; 2016 Treehouse Island, Inc.
+      &copy; 2017 <?php echo $name; ?>
     </section>
   </body>
 </html>
